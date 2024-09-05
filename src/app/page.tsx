55b461321b03f0c1e98573689a575aba223d6f31
BNsrujan <srujanbn123@gmail.com>
2024-09-05 16:33:03 +0530

    created nextJs with shadcn and Lightweight Charts

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button"
+export default function Home() {
+  return (
+      <div className="flex justify-center ">
+      <Button className="px-4 py-2 text-base sm:text-lg lg:text-xl ">Click Me</Button>
+    </div>
+  );
+}
